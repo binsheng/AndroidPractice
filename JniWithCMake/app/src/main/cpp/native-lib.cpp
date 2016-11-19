1fbe5ac07a09bf89417b49e1d01ad8ecbc286aa9
bin <bin6160@126.com>
2016-11-19 22:02:07 +0800

    在cpp文件里需要把生成的方法包含在extern "C" {}里面

--- JniWithCMake/app/src/main/cpp/native-lib.cpp
+++ JniWithCMake/app/src/main/cpp/native-lib.cpp
@@ -1,12 +1,13 @@
 #include <jni.h>
 #include <string>
-extern "C"
-jint
+
+extern "C" {
+
+JNIEXPORT jint JNICALL
 Java_com_dev_bins_jniwithcmake_MainActivity_add(JNIEnv *env, jobject instance, jint x, jint y) {
-    return x+y;
+    return x + y;
 }
 
-extern "C"
 jstring
 Java_com_dev_bins_jniwithcmake_MainActivity_stringFromJNI(
         JNIEnv *env,
@@ -14,3 +15,7 @@ Java_com_dev_bins_jniwithcmake_MainActivity_stringFromJNI(
     std::string hello = "JniWithCMake";
     return env->NewStringUTF(hello.c_str());
 }
+
+}
+
+
