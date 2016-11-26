15cbe4e9e0f28cc771ccde05efe33b911216a718
bin <bin6160@126.com>
2016-11-26 21:39:35 +0800

    HexagonLayoutManager complete

--- HexagonLayoutManager/app/src/main/java/com/dev/bins/hexagonlayoutmanager/HexagonAdapter.java
+++ HexagonLayoutManager/app/src/main/java/com/dev/bins/hexagonlayoutmanager/HexagonAdapter.java
@@ -1,8 +1,11 @@
 package com.dev.bins.hexagonlayoutmanager;
 
+import android.graphics.Bitmap;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import java.util.List;
 
@@ -12,30 +15,33 @@ import java.util.List;
 
 public class HexagonAdapter extends RecyclerView.Adapter<HexagonAdapter.Holder> {
 
-    List<> data
+    List<Bitmap> data;
 
-    public HexagonAdapter() {
+    public HexagonAdapter(List<Bitmap> data) {
+        this.data = data;
     }
 
     @Override
     public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        return new Holder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false));
     }
 
     @Override
     public void onBindViewHolder(Holder holder, int position) {
-
+        holder.imageView.setImageDrawable(new HexagonDrawable(data.get(position)));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return data.size();
     }
 
-    class Holder extends RecyclerView.ViewHolder{
+    class Holder extends RecyclerView.ViewHolder {
+        ImageView imageView;
 
         public Holder(View itemView) {
             super(itemView);
+            imageView = (ImageView) itemView.findViewById(R.id.iv);
         }
     }
 }
--- HexagonLayoutManager/app/src/main/java/com/dev/bins/hexagonlayoutmanager/MainActivity.java
+++ HexagonLayoutManager/app/src/main/java/com/dev/bins/hexagonlayoutmanager/MainActivity.java
@@ -1,16 +1,54 @@
 package com.dev.bins.hexagonlayoutmanager;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
+    private HexagonAdapter mAdapter;
+    private List<Bitmap> bitmapList = new ArrayList<>();
+    private Bitmap bitmap;
+    private Random random;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler);
-
+        mAdapter = new HexagonAdapter(bitmapList);
+        mRecyclerView.setLayoutManager(new HexagonLayoutManager());
+        mRecyclerView.setAdapter(mAdapter);
+        random = new Random();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.img);
+        for (int i = 0; i < 7; i++) {
+            bitmapList.add(bitmap);
+        }
+        mAdapter.notifyDataSetChanged();
+    }
+
+    public void add(View view) {
+        bitmapList.add(bitmap);
+        mAdapter.notifyItemInserted(random.nextInt(bitmapList.size() - 1));
+    }
+
+    public void del(View view) {
+        int i = random.nextInt(bitmapList.size() - 1);
+        bitmapList.remove(i);
+        mAdapter.notifyItemRemoved(i);
+    }
+
+
 }
