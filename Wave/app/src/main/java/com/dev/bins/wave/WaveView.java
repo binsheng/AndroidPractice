5b9925a71f16a8d2b8f1e8573cfb3431d016a679
bin <bin6160@126.com>
2016-12-12 20:43:14 +0800

    init WaveView

---
+++ Wave/app/src/main/java/com/dev/bins/wave/WaveView.java
@@ -0,0 +1,23 @@
+package com.dev.bins.wave;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by bin on 12/12/2016.
+ */
+
+public class WaveView extends View {
+    public WaveView(Context context) {
+        super(context);
+    }
+
+    public WaveView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public WaveView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
