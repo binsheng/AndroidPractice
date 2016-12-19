a7e243728adb6e9466391ab957f59bc93b5d5b2e
bin <bin6160@126.com>
2016-12-19 22:10:44 +0800

    base vector anim

--- VectorDrawable/app/src/main/java/com/dev/bins/vectordrawable/MainActivity.java
+++ VectorDrawable/app/src/main/java/com/dev/bins/vectordrawable/MainActivity.java
@@ -1,8 +1,12 @@
 package com.dev.bins.vectordrawable;
 
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDelegate;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +19,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void anim(View view) {
+        ImageView imageView = (ImageView) view;
+        Drawable drawable = imageView.getDrawable();
+        if (drawable instanceof Animatable) {
+            ((Animatable) drawable).start();
+        }
     }
 }
