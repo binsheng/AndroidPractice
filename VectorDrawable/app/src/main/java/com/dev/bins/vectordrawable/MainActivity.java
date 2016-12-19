fa43c63e6df31e91c8faec65aea07dcf8eb2bb73
bin <bin6160@126.com>
2016-12-19 21:22:55 +0800

    base use

--- VectorDrawable/app/src/main/java/com/dev/bins/vectordrawable/MainActivity.java
+++ VectorDrawable/app/src/main/java/com/dev/bins/vectordrawable/MainActivity.java
@@ -2,8 +2,14 @@ package com.dev.bins.vectordrawable;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
