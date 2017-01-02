a10d2c8824f6f2ffadbd634f570059085c0fd93b
bin <bin6160@126.com>
2017-01-02 22:00:22 +0800

    suspend come

--- RecyclerViewItemDecoration/app/src/main/java/com/dev/bins/recyclerviewitemdecoration/SuspendingDecoration.java
+++ RecyclerViewItemDecoration/app/src/main/java/com/dev/bins/recyclerviewitemdecoration/SuspendingDecoration.java
@@ -4,7 +4,9 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.SoundEffectConstants;
 import android.view.View;
 
 import java.util.List;
@@ -53,15 +55,49 @@ public class SuspendingDecoration extends RecyclerView.ItemDecoration {
         textPaint.setTextSize(50);
         for (int i = 0; i < parent.getChildCount(); i++) {
             View childAt = parent.getChildAt(i);
-            int pos = ((RecyclerView.LayoutParams)childAt.getLayoutParams()).getViewLayoutPosition();
+            int pos = ((RecyclerView.LayoutParams) childAt.getLayoutParams()).getViewLayoutPosition();
             if (i == 0) {
                 c.drawRect(0, childAt.getTop() - 50, parent.getWidth(), childAt.getTop(), paint);
-                c.drawText(mDatas.get(pos).getTag(), 10, childAt.getTop() - 20, textPaint);
+                c.drawText(mDatas.get(pos).getTag(), 10, childAt.getTop() - 10, textPaint);
             } else if (!mDatas.get(pos).getTag().equals(mDatas.get(pos - 1).getTag())) {
                 c.drawRect(0, childAt.getTop() - 50, parent.getWidth(), childAt.getTop(), paint);
-                c.drawText(mDatas.get(pos).getTag(), 10, childAt.getTop() - 20, textPaint);
+                c.drawText(mDatas.get(pos).getTag(), 10, childAt.getTop() - 10, textPaint);
             }
         }
+    }
+
+
+    @Override
+    public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        super.onDrawOver(c, parent, state);
+
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setDither(true);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setStrokeWidth(20);
+        paint.setColor(Color.GRAY);
+        Paint textPaint = new Paint();
+        textPaint.setAntiAlias(true);
+        textPaint.setDither(true);
+        textPaint.setStyle(Paint.Style.FILL);
+        textPaint.setStrokeWidth(20);
+        textPaint.setColor(Color.BLACK);
+        textPaint.setTextSize(50);
+
+        int pos = ((LinearLayoutManager) parent.getLayoutManager()).findFirstVisibleItemPosition();
+        View view = parent.findViewHolderForLayoutPosition(pos).itemView;
+        c.save();
+        if (!mDatas.get(pos).getTag().equals(mDatas.get(pos+1).getTag())){
+            if (view.getBottom() < height) {//view.getTop() + view.getHeight()和getbottom的效果是一样的
+                System.out.println(view.getTop());
+                System.out.println(view.getHeight());
+                c.translate(0, view.getTop() + view.getHeight() - height);
+            }
+        }
+        c.drawRect(0, parent.getTop(), parent.getWidth(), parent.getTop()+height, paint);
+        c.drawText(mDatas.get(pos).getTag(), 10, 35, textPaint);
+        c.restore();
 
     }
 }
