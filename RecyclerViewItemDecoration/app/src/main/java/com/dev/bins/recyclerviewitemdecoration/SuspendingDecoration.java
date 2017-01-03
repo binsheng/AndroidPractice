212fa449c150674cc43be177f8be936b7abcdec7
bin <bin6160@126.com>
2017-01-03 22:01:58 +0800

    refactor paint

--- RecyclerViewItemDecoration/app/src/main/java/com/dev/bins/recyclerviewitemdecoration/SuspendingDecoration.java
+++ RecyclerViewItemDecoration/app/src/main/java/com/dev/bins/recyclerviewitemdecoration/SuspendingDecoration.java
@@ -6,7 +6,6 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.SoundEffectConstants;
 import android.view.View;
 
 import java.util.List;
@@ -18,9 +17,29 @@ import java.util.List;
 public class SuspendingDecoration extends RecyclerView.ItemDecoration {
     private List<CityBean> mDatas;
     private int height = 50;
+    private Paint mPaint;
+    private Paint mTextPaint;
 
     public SuspendingDecoration(List<CityBean> mDatas) {
         this.mDatas = mDatas;
+        initPaint();
+    }
+
+
+    private void initPaint() {
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setStrokeWidth(20);
+        mPaint.setColor(Color.GRAY);
+        mTextPaint = new Paint();
+        mTextPaint.setAntiAlias(true);
+        mTextPaint.setDither(true);
+        mTextPaint.setStyle(Paint.Style.FILL);
+        mTextPaint.setStrokeWidth(20);
+        mTextPaint.setColor(Color.BLACK);
+        mTextPaint.setTextSize(50);
     }
 
     @Override
@@ -40,28 +59,16 @@ public class SuspendingDecoration extends RecyclerView.ItemDecoration {
     @Override
     public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
         super.onDraw(c, parent, state);
-        Paint paint = new Paint();
-        paint.setAntiAlias(true);
-        paint.setDither(true);
-        paint.setStyle(Paint.Style.FILL);
-        paint.setStrokeWidth(20);
-        paint.setColor(Color.GRAY);
-        Paint textPaint = new Paint();
-        textPaint.setAntiAlias(true);
-        textPaint.setDither(true);
-        textPaint.setStyle(Paint.Style.FILL);
-        textPaint.setStrokeWidth(20);
-        textPaint.setColor(Color.BLACK);
-        textPaint.setTextSize(50);
+
         for (int i = 0; i < parent.getChildCount(); i++) {
             View childAt = parent.getChildAt(i);
             int pos = ((RecyclerView.LayoutParams) childAt.getLayoutParams()).getViewLayoutPosition();
             if (i == 0) {
-                c.drawRect(0, childAt.getTop() - 50, parent.getWidth(), childAt.getTop(), paint);
-                c.drawText(mDatas.get(pos).getTag(), 10, childAt.getTop() - 10, textPaint);
+                c.drawRect(0, childAt.getTop() - 50, parent.getWidth(), childAt.getTop(), mPaint);
+                c.drawText(mDatas.get(pos).getTag(), 10, childAt.getTop() - 10, mTextPaint);
             } else if (!mDatas.get(pos).getTag().equals(mDatas.get(pos - 1).getTag())) {
-                c.drawRect(0, childAt.getTop() - 50, parent.getWidth(), childAt.getTop(), paint);
-                c.drawText(mDatas.get(pos).getTag(), 10, childAt.getTop() - 10, textPaint);
+                c.drawRect(0, childAt.getTop() - 50, parent.getWidth(), childAt.getTop(), mPaint);
+                c.drawText(mDatas.get(pos).getTag(), 10, childAt.getTop() - 10, mTextPaint);
             }
         }
     }
@@ -71,33 +78,22 @@ public class SuspendingDecoration extends RecyclerView.ItemDecoration {
     public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
         super.onDrawOver(c, parent, state);
 
-        Paint paint = new Paint();
-        paint.setAntiAlias(true);
-        paint.setDither(true);
-        paint.setStyle(Paint.Style.FILL);
-        paint.setStrokeWidth(20);
-        paint.setColor(Color.GRAY);
-        Paint textPaint = new Paint();
-        textPaint.setAntiAlias(true);
-        textPaint.setDither(true);
-        textPaint.setStyle(Paint.Style.FILL);
-        textPaint.setStrokeWidth(20);
-        textPaint.setColor(Color.BLACK);
-        textPaint.setTextSize(50);
 
         int pos = ((LinearLayoutManager) parent.getLayoutManager()).findFirstVisibleItemPosition();
         View view = parent.findViewHolderForLayoutPosition(pos).itemView;
         c.save();
-        if (!mDatas.get(pos).getTag().equals(mDatas.get(pos+1).getTag())){
+        if (!mDatas.get(pos).getTag().equals(mDatas.get(pos + 1).getTag())) {
             if (view.getBottom() < height) {//view.getTop() + view.getHeight()和getbottom的效果是一样的
                 System.out.println(view.getTop());
                 System.out.println(view.getHeight());
                 c.translate(0, view.getTop() + view.getHeight() - height);
             }
         }
-        c.drawRect(0, parent.getTop(), parent.getWidth(), parent.getTop()+height, paint);
-        c.drawText(mDatas.get(pos).getTag(), 10, 35, textPaint);
+        c.drawRect(0, parent.getTop(), parent.getWidth(), parent.getTop() + height, mPaint);
+        c.drawText(mDatas.get(pos).getTag(), 10, 35, mTextPaint);
         c.restore();
 
     }
+
+
 }
