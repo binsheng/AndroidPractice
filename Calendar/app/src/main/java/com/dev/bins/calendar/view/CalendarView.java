bcc45551f7861dfaa3c82605e2fe8fb3cd9d4b23
bin <bin6160@126.com>
2017-10-10 14:36:36 +0800

    update README

--- Calendar/app/src/main/java/com/dev/bins/calendar/view/CalendarView.java
+++ Calendar/app/src/main/java/com/dev/bins/calendar/view/CalendarView.java
@@ -2,7 +2,6 @@ package com.dev.bins.calendar.view;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
