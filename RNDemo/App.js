594b465f0dcefac56dc951fe200d44173fb73a57
bin <bin6160@126.com>
2017-10-21 18:26:49 +0800

    add text

--- RNDemo/App.js
+++ RNDemo/App.js
@@ -1,48 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-
+import LifecycleComponent from './LifecycleComponent'
 
 
 export default class App extends React.Component {
 
-
-  static defaultProps = {
-
-  };
-
-
-
-  constructor(props){
-      super(props)
-  }
-
-
-  componentWillMount(){
-
-  }
 
 
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+          <LifecycleComponent/>
       </View>
     );
   }
 
-  componentDidMount(){
-
-  }
-
-  componentWillReceiveProps(){
-
-  }
-
-  
 
 }
 
@@ -50,7 +24,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
+
   },
 });
--- RNDemo/LifecycleComponent.js
+++ RNDemo/LifecycleComponent.js
@@ -1,17 +1,21 @@
 
 
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View ,Text } from 'react-native';
 
 
 export default class LifecycleComponent extends React.Component{
 
 
+    static defaultProps = {
+        componentName:"LifecycleComponet"
+    }
+
     constructor(props){
         super(props)
         console.log("constructor")
+        this.state = {count:0}
     }
-
 
     componentWillMount(){
         console.log("componentWillMount")
@@ -30,14 +34,13 @@ export default class LifecycleComponent extends React.Component{
         return true;
     }
 
-
     componentWillUpdate(nextProps,nextState){
         console.log("componentWillUpdate")
     }
+
     componentDidUpdate(nextProps,nextState){
         console.log("componentDidUpdate")
     }
-
 
     componentWillUnmount(){
         console.log("componentWillUnmout")
@@ -46,8 +49,12 @@ export default class LifecycleComponent extends React.Component{
     render(){
         console.log("render")
         return(
-            <View>
-                LifecycleComponent
+            <View style={{backgroundColor:'red'}} >
+                <Text style={{fontSize:30}} onPress={()=>{
+                console.log("点击")
+                this.setState({count:this.state.count+1})
+            }}>LifecycleComponent</Text>
+                <Text style={{fontSize:30}}>点击了{this.state.count}次！</Text>
             </View>
         );
     }
